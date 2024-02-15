5c665c46188207a0679797ba49d03b30d4ca7b0f
Kaushalya Hulathdoowage <skdilukshi97@gmail.com>
2024-02-15 23:06:56 +0530

    commit changes

--- Vehical_Number_Plate_Detector/project3.py
+++ Vehical_Number_Plate_Detector/project3.py
@@ -15,8 +15,12 @@ count = 0
 
 while True:
     success, img = cap.read()
+    if not success:
+        break
+
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     numberPlates = nPlateCascade.detectMultiScale(imgGray, 1.1, 10)
+
     for (x, y, w, h) in numberPlates:
         area = w * h
         if area > minArea:
@@ -26,13 +30,19 @@ while True:
             imgRoi = img[y:y + h, x:x + w]
             cv2.imshow("ROI", imgRoi)
 
+            if cv2.waitKey(1) & 0xFF == ord('s'):
+                cv2.imwrite("Resources/Scanned/NoPlate_" + str(count) + ".jpg", imgRoi)
+                cv2.rectangle(img, (0, 200), (640, 300), (0, 255, 0), cv2.FILLED)
+                cv2.putText(img, "Scan Saved", (150, 265), cv2.FONT_HERSHEY_DUPLEX,
+                            2, (0, 0, 255), 2)
+                cv2.imshow("Result", img)
+                cv2.waitKey(500)
+                count += 1
+
     cv2.imshow("Result", img)
 
-    if cv2.waitKey(1) & 0xFF == ord('s'):
-        cv2.imwrite("Resources/Scanned/NoPlate_" + str(count) + ".jpg", imgRoi)
-        cv2.rectangle(img, (0, 200), (640, 300), (0, 255, 0), cv2.FILLED)
-        cv2.putText(img, "Scan Saved", (150, 265), cv2.FONT_HERSHEY_DUPLEX,
-                    2, (0, 0, 255), 2)
-        cv2.imshow("Result", img)
-        cv2.waitKey(500)
-        count += 1
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
